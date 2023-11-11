b8971ed5b7c9b29156bbfad661e257dab0c28ae2
Aldo Ribeiro <aldocred>
2023-11-11 17:09:03 -0300

    iniciado criação da classe post de blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from datetime import datetime
+
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    autor = models.CharField(max_length =255)
+    titulo = models.CharField(max_length =255)
+    subtitulo = models.CharField(max_length =255)
+    conteudo = models.CharField()
+    data_publicacao = models.DateTimeField(default = datetime.now())
