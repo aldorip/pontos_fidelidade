391ae7a7d50293daba1f3c371af28790428e732c
Aldo Ribeiro <aldocred>
2023-11-11 17:40:29 -0300

    Class Post charfild par texfild e conn postgres

--- blog/models.py
+++ blog/models.py
@@ -8,5 +8,5 @@ class Post(models.Model):
     autor = models.CharField(max_length =255)
     titulo = models.CharField(max_length =255)
     subtitulo = models.CharField(max_length =255)
-    conteudo = models.CharField()
+    conteudo = models.TextField()
     data_publicacao = models.DateTimeField(default = datetime.now())
--- core/settings.py
+++ core/settings.py
@@ -77,7 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'arp3966',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
